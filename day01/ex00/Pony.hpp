80cb449a8afa9a578801f31384d4cc21ed3bfc8f
Alona KORZHAKOVA <akorzhak@e1r8p11.unit.ua>
2018-10-02 18:09:33 +0300

    tmp

---
+++ day01/ex00/Pony.hpp
@@ -0,0 +1,21 @@
+#ifndef PONY_HPP
+# define PONY_HPP
+
+# include <iostream>
+# include <string>
+
+/*
+****************************** PONY CLASS **************************************
+*/
+
+class Pony
+{
+	private:
+
+		bool		fluffy;
+		int			weight;
+		int			age;
+		std::string	breed;
+		std::string	color;
+		std::string	name;
+}
